1b553496a0e6b2990d5b2fdde7c759cf5bd8c386
udohchuks <urichard132@gmail.com>
2023-03-16 18:02:14 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry value
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char: %u byte(s)\n", sizeof(char));
+	printf("Size of int: %u byte(s)\n", sizeof(int));
+	printf("Size of long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of float: %u byte(s)\n", sizeof(float));
+}

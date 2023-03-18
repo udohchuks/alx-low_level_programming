07382898f1ae324aacf0887e43fd6e203586accb
udohchuks <urichard132@gmail.com>
2023-03-17 17:29:39 -0700

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <=  'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

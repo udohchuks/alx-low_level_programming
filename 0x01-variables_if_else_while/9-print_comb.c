ce22308acecdf6ceeb3b799f8da7fc8835253b36
udohchuks <urichard132@gmail.com>
2023-03-17 18:53:47 -0700

    repaired

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,10 +4,11 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int a;
-	
+
 	for (a = 0; a <= 9 ; a++)
 	{
 		putchar(48 + a);
@@ -20,4 +21,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}

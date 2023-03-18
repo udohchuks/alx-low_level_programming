8b6ddf90cf88cebfbede3a5650c45770f2d38d62
udohchuks <urichard132@gmail.com>
2023-03-17 17:48:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a  < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}

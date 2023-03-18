fd78c4a4d306640ecc64914f62b3079356f7b8d9
udohchuks <urichard132@gmail.com>
2023-03-17 19:23:49 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of two single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			if (i == 8)
+				break;
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

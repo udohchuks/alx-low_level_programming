ae323efa8a13240575ed92ff45f73877ca26d984
udohchuks <urichard132@gmail.com>
2023-03-22 20:17:25 -0700

    natural

---
+++ 0x02-functions_nested_loops/12-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the sum of multiple
+ * Return: 0
+ */
+int main(void)
+{
+	int Total;
+
+	Total = multiple(1024);
+	printf("%d\n", Total);
+	return (0);
+}

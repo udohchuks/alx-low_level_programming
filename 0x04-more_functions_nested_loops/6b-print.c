37f19d8fc9483f0608c99f01c23468497b7a4bd9
udohchuks <urichard132@gmail.com>
2023-03-24 16:04:29 +0100

    diagonal

---
+++ 0x04-more_functions_nested_loops/6b-print.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints _
+ * @n: integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonally
+ * @n: integer
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+}

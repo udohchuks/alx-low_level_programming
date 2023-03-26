b00a9ae133b3f1060010d37765686244ec189d6e
udohchuks <urichard132@gmail.com>
2023-03-26 12:52:42 +0100

    first 98 fib number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - first fib numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int x, y, z;
+
+	int n;
+
+	x = 1;
+	y = 2;
+	n = 98;
+	printf("%lu, %lu", x, y);
+	while (n >= 1)
+	{
+		z = x + y;
+		printf(", %lu", z);
+		x = y;
+		y = z;
+		n -= 1;
+	}
+	printf("\n");
+	return (0);
+}
+

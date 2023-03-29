623c3d9e08103856fe258920ff48f555b20db6a0
udohchuks <urichard132@gmail.com>
2023-03-29 14:04:53 +0100

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcat  - pointer functio that concatenates two strings
+ * @dest: pointer destination string
+ * @src: pointer source string
+ * Return: String
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}

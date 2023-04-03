a934cae935819dfab736cc9529aa930628e4b3a8
udohchuks <urichard132@gmail.com>
2023-04-03 15:55:01 +0100

    add description for n in 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - copies src to dest for n char
  * @dest: destination pointer
  * @src: source pointer
- * n: number of character
+ * @n: number of character
  * Return: pointer
  */
 char *_memcpy(char *dest, char *src, unsigned int n)

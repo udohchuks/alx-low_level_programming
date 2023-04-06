ed05278bb3d46854072b7663330bbed2fe046e83
udohchuks <urichard132@gmail.com>
2023-04-06 09:14:08 +0100

    check palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * palindrome - checks palindrome
+ * @s: pointer string
+ * @high: length -1
+ * @low: 0
+ * Return: 0 or 1
+ */
+int palindrome(char *s, int high, int low)
+{
+	if (low >= high)
+	{
+		return (1);
+	}
+	else if (s[low] != s[high])
+	{
+		return (0);
+	}
+	else
+	{
+		return (palindrome(s, high - 1, low + 1));
+	}
+}
+/**
  * is_palindrome - checks palindrome
  * @s: pointer to a string
  * Return: 1 for true and 0 for false
@@ -7,23 +29,15 @@
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
-	int i, check;
+	int low = 0;
+	int high = length - 1;
 
-	check = 1;
-	for (i = 0; i <= length / 2; i++)
-	{
-		if (s[i] != s[length - 1 - i])
-		{
-			check = 0;
-			break;
-		}
-	}
-	if (check)
+	if (length == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return (0);
+		return (palindrome(s, high, low));
 	}
 }

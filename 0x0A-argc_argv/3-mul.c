16916659983f793a589d11af5427a96bc4d1da40
udohchuks <urichard132@gmail.com>
2023-04-09 23:36:49 +0100

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arg vector
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i, prod, value;
+
+	if (argc > 1)
+	{
+		prod = 1;
+		for (i = 1; i < argc; i++)
+		{
+			value = atoi(argv[i]);
+			prod *= value;
+		}
+		printf("%d\n", prod);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}

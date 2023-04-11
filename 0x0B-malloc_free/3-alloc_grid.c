75df295eba934b186db5edfa6fccb0375898c16e
udohchuks <urichard132@gmail.com>
2023-04-11 22:12:46 +0100

    2d

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		p[i] = (int *)malloc(sizeof(int) * width);
+	}
+	for (i = 0; i < height; i++)
+	{
 		if (p[i] == NULL)
 		{
 			while (i >= 0)

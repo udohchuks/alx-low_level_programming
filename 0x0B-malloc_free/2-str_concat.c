e3a5567b3f429c41fe985708c4ffde44fb5c9854
udohchuks <urichard132@gmail.com>
2023-04-11 20:49:00 +0100

    concatanate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,18 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int length;
 
+	if (*s1 == 0)
+	{
+		s1 = "";
+	}
+	if (*s2 == 0)
+	{
+		s2 = "";
+	}
 	length = strlen(s1) + strlen(s2);
 	str = malloc(sizeof(char) * length + 1);
 	strcat(str, s1);
-	strcat(str,s2);
+	strcat(str, s2);
 	str[length + 1] = '\0';
 	return (str);
 }

1ee67a28bcfddcc0e1c7ab9f0e144d86878e849a
udohchuks <urichard132@gmail.com>
2023-04-14 10:12:18 +0100

    cat 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len2;
-	int i, len1, length;
+	unsigned int  j;
+	int i, len1 = 0, length;
 	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
 	length = len1 + n + 1;
 	str = malloc(length * sizeof(char));
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= len1; i++)
+	for (i = 0; s1[i] !=  '\0'; i++)
 		str[i] = s1[i];
-	if (n >= len2)
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
 	{
-		strcat(str, s2);
+		str[i] = s2[j];
 	}
-	else
-	{
-		strncat(str, s2, n);
-	}
-	printf("%ld", sizeof(str));
+	str[i] = '\0';
 	return (str);
 }

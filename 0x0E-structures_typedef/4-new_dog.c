6f98e9a3133c90a2f70967a9b1896978cd26154f
udohchuks <urichard132@gmail.com>
2023-04-17 20:17:46 +0100

    Write a function that frees dogs.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,11 +36,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * _strlen(name) + 1);
 	if (dog->name == NULL)
 	{
+		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 	dog->name = name;

e2ef7686980c1baebc4b205db75ea1d310f0f14e
udohchuks <urichard132@gmail.com>
2023-04-30 17:31:40 +0100

    fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,11 @@ void free_list(list_t *head)
 {
 	list_t *ptr;
 
-	if (head == NULL)
-		return;
-	ptr = head;
 	while (ptr != NULL)
 	{
 		ptr = ptr->next;
 		free(ptr);
 		free(ptr->str);
 	}
+	free(head);
 }

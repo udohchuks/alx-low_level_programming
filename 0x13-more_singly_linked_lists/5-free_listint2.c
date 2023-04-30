93a75dc9989af00eb37bf95af38af04810ae88e8
udohchuks <urichard132@gmail.com>
2023-04-30 17:55:59 +0100

    5 free mem

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free memory
+ * @head: pointer pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = *head->next;
+		free(ptr);
+	}
+	free(*head);
+	*head = NULL;
+}

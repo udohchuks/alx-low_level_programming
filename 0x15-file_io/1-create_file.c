0999e3fcd6544fe6da0c0ed9669baa4f0ee5dd61
udohchuks <urichard132@gmail.com>
2023-05-09 02:31:34 +0100

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-create_file.c -o b

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,17 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	
+	while (text_content[size] != '\0')
+		size++;
 	if (text_content != NULL)
 	{
-		text_content = "";
-	}
-	while (text_content[size])
-		size++;
-	nwrite = write(fd, text_content, size);
-	if (nwrite == -1)
-	{
-		close(fd);
-		return (-1);
+		nwrite = write(fd, text_content, size);
+		if (nwrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);

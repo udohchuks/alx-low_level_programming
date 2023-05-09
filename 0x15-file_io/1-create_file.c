fc71e1840df4aca92924c45439da41709719f21a
udohchuks <urichard132@gmail.com>
2023-05-09 02:23:42 +0100

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-create_file.c -o b

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	size = sizeof(text_content) - 1;
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT |O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)

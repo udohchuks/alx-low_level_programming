768b03b2fd5ae558a12470d60aad523de764b5f2
udohchuks <urichard132@gmail.com>
2023-06-16 21:05:04 +0100

    task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - free lists
+ * @head: head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	if (head)
+	{
+		while (head->next != NULL)
+		{
+			current = head;
+			head = head->next;
+			free(current);
+		}
+	}
+}

696ee064bf26ac524288d10b3a391126988df7e1
udohchuks <urichard132@gmail.com>
2023-07-07 23:47:24 +0100

    fix-tsk0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	long unsigned  int i;
-	hash_table_t* table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
 	table->size = size;
-	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
+	{
+		fprintf(stderr, "Error");
+		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
-	return table;
+
+	return (table);
 }
